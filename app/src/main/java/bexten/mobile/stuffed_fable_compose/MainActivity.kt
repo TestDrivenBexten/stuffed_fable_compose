90188ff8a7244bfbff2e4edb9b9d800f4a7d0d60
Skyler Bexten <skyler.bexten@gmail.com>
2024-09-17 20:23:50 -0700

    Initial Stuffed Fables example

---
+++ app/src/main/java/bexten/mobile/stuffed_fable_compose/MainActivity.kt
@@ -0,0 +1,69 @@
+package bexten.mobile.stuffed_fable_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import bexten.mobile.stuffed_fable_compose.ui.theme.Stuffed_fable_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Stuffed_fable_composeTheme {
+                StuffedFablesHeader()
+            }
+        }
+    }
+}
+
+@Composable
+fun StuffedFablesHeader() {
+    Text(text = "Stuffed Fables Dice Draw Calculator")
+}
+
+@Composable
+fun DieButton() {
+    Button(onClick = {}, shape = RoundedCornerShape(4.dp)) {
+        Text(text = "1")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DieButtonPreview() {
+    Stuffed_fable_composeTheme {
+        DieButton()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StuffedFablesHeaderPreview() {
+    Stuffed_fable_composeTheme {
+        StuffedFablesHeader()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceDrawnPreview() {
+    Stuffed_fable_composeTheme {
+        Row {
+            DieButton()
+            DieButton()
+            DieButton()
+            DieButton()
+            DieButton()
+        }
+    }
+}
