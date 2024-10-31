3e0b858eb37f0f9831e93460d7be963d88f3f368
Skyler Bexten <skyler.bexten@gmail.com>
2024-10-30 19:14:30 -0700

    Cap dice to draw from the bag

--- app/src/main/java/bexten/mobile/stuffed_fable_compose/DiceDraw.kt
+++ app/src/main/java/bexten/mobile/stuffed_fable_compose/DiceDraw.kt
@@ -1,8 +1,11 @@
 package bexten.mobile.stuffed_fable_compose
 
+import kotlin.math.min
+
 data class DiceDraw(val totalDiceInBag: Int, val totalDiceToDraw: Int)
 
 fun getDiceDraw(diceBag: DiceBag, totalDiceToDraw: Int): DiceDraw {
     val totalDiceInBag = diceBag.diceSelectionList.sumOf { it.maxDice - it.selectedCount }
-    return DiceDraw(totalDiceInBag, totalDiceToDraw)
+    val toDrawCount = min(totalDiceToDraw, totalDiceInBag)
+    return DiceDraw(totalDiceInBag, toDrawCount)
 }
--- app/src/test/java/bexten/mobile/stuffed_fable_compose/DiceDrawTest.kt
+++ app/src/test/java/bexten/mobile/stuffed_fable_compose/DiceDrawTest.kt
@@ -43,4 +43,24 @@ class DiceDrawTest {
         assertEquals(5, diceDraw.totalDiceInBag)
         assertEquals(5, diceDraw.totalDiceToDraw)
     }
+
+    @Test
+    fun shouldHaveDiceToDrawBeLessThanOrEqualToTotalDiceFromBag() {
+        // Arrange
+        val blackDie = createD6Die(DieColor.BLACK)
+        val greenDie = createD6Die(DieColor.GREEN)
+        val diceBag = DiceBag(
+            diceSelectionList = listOf(
+                DiceSelection(blackDie, 4, 0,5, ComparisonOperator.NONE),
+                DiceSelection(greenDie, 3, 0,5, ComparisonOperator.NONE),
+            )
+        )
+
+        // Act
+        val diceDraw = getDiceDraw(diceBag, 5)
+
+        // Assert
+        assertEquals(3, diceDraw.totalDiceInBag)
+        assertEquals(3, diceDraw.totalDiceToDraw)
+    }
 }
